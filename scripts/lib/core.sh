71e93028b34577f65c89fde83281586e966b9cd2
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-20 08:45:49 +0600

    fix(cli): restore wrapper version assignment

--- scripts/lib/core.sh
+++ scripts/lib/core.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC2034 # consumed by sourced command modules\nVERSION="${LLM_FASTFLOW_VERSION:-dev}"
+# shellcheck disable=SC2034
+VERSION="${LLM_FASTFLOW_VERSION:-dev}"
 DEFAULT_MODEL="qwen3.5:9b"
 FLM_BIN="${FLM_BIN:-/opt/fastflowlm/flm}"
 
